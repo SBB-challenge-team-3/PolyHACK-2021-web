a0b31a91342d5645ea36e061b0a707d0ae9099c3
samuel0007 <russo.samuel20@gmail.com>
2021-10-30 20:35:39 +0200

    add basic wb

---
+++ my-svelte-project/src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import Button from "./Button.svelte";
+  import Logo from "./Logo.svelte";
+  import Tailwind from "./Tailwind.svelte";
+</script>
+
+<Tailwind />
+
+<main class="font-headline flex items-center flex-col">
+  <div class="bg-red-500 w-full text-white text-5xl p-10 text-center">SBB</div>
+  <p class="text-center p-10">Test paragraph</p>
+  <div class="w-20">
+    <Button>A Button</Button>
+  </div>
+</main>
